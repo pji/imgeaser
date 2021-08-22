690a654d1faf6c26b39411408265c64199e5a5f5
Paul J. Iutzi <pji@mac.com>
2021-08-21 19:21:14 -0500

    Added the basic ease in functions.

--- imgeaser/imgeaser.py
+++ imgeaser/imgeaser.py
@@ -11,6 +11,43 @@ from imgeaser.utility import will_scale
 
 # Ease in functions.
 @will_scale
+def ease_in_back(a: np.ndarray) -> np.ndarray:
+    c1 = 1.70158
+    c3 = c1 + 1
+    return c3 * a ** 3 - c1 * a ** 2
+
+
+@will_scale
+def ease_in_circ(a: np.ndarray) -> np.ndarray:
+    return 1 - np.sqrt(1 - a ** 2)
+
+
+@will_scale
+def ease_in_cubic(a: np.ndarray) -> np.ndarray:
+    return a ** 3
+
+
+@will_scale
+def ease_in_elastic(a: np.ndarray) -> np.ndarray:
+    c4 = (2 * np.pi) / 3
+    m = np.zeros(a.shape, bool)
+    m[a == 0] = True
+    m[a == 1] = True
+    a[~m] = -(2 ** (10 * a[~m] - 10)) * np.sin((a[~m] * 10 - 10.75) * c4)
+    return a
+
+
+@will_scale
+def ease_in_quad(a: np.ndarray) -> np.ndarray:
+    return a ** 2
+
+
+@will_scale
+def ease_in_quint(a: np.ndarray) -> np.ndarray:
+    return a ** 5
+
+
+@will_scale
 def ease_in_sin(a: np.ndarray) -> np.ndarray:
     return 1 - np.cos(a * np.pi / 2)
 
@@ -22,6 +59,7 @@ def ease_out_sin(a: np.ndarray) -> np.ndarray:
 
 
 # Ease in and out functions.
+@will_scale
 def ease_in_out_sin(a: np.ndarray) -> np.ndarray:
     return -1 * (np.cos(np.pi * a) - 1) / 2
 
@@ -29,7 +67,7 @@ def ease_in_out_sin(a: np.ndarray) -> np.ndarray:
 if __name__ == '__main__':
     from imgeaser.utility import print_array, A
     
-    ease = ease_in_out_sin
+    ease = ease_in_quint
     a = A.copy()
     eased = ease(a)
     print_array(eased, depth=2)
--- tests/test_imgeaser.py
+++ tests/test_imgeaser.py
@@ -25,9 +25,117 @@ class EaseTestCase(ArrayTestCase):
 
 
 # Test case.
+class EaseInBackTestCase(EaseTestCase):
+    def test_ease(self):
+        """Given an array of image data, run the 'in back' easing
+        function on the data and return the result.
+        """
+        ease = ie.ease_in_back
+        exp = np.array([
+            [
+                [0.0000, -0.0641, -0.0877, 0.1826, 1.0000],
+                [-0.0641, -0.0877, 0.1826, 1.0000, 0.1826],
+                [-0.0877, 0.1826, 1.0000, 0.1826, -0.0877],
+                [0.1826, 1.0000, 0.1826, -0.0877, -0.0641],
+                [1.0000, 0.1826, -0.0877, -0.0641, 0.0000],
+            ],
+        ], dtype=np.float32)
+        self.run_test(exp, ease)
+
+
+class EaseInCircTestCase(EaseTestCase):
+    def test_ease(self):
+        """Given an array of image data, run the 'in circ' easing
+        function on the data and return the result.
+        """
+        ease = ie.ease_in_circ
+        exp = np.array([
+            [
+                [0.0000, 0.0318, 0.1340, 0.3386, 1.0000],
+                [0.0318, 0.1340, 0.3386, 1.0000, 0.3386],
+                [0.1340, 0.3386, 1.0000, 0.3386, 0.1340],
+                [0.3386, 1.0000, 0.3386, 0.1340, 0.0318],
+                [1.0000, 0.3386, 0.1340, 0.0318, 0.0000],
+            ],
+        ], dtype=np.float32)
+        self.run_test(exp, ease)
+
+
+class EaseInCubicTestCase(EaseTestCase):
+    def test_ease(self):
+        """Given an array of image data, run the 'in cubic' easing
+        function on the data and return the result.
+        """
+        ease = ie.ease_in_cubic
+        exp = np.array([
+            [
+                [0.0000, 0.0156, 0.1250, 0.4219, 1.0000],
+                [0.0156, 0.1250, 0.4219, 1.0000, 0.4219],
+                [0.1250, 0.4219, 1.0000, 0.4219, 0.1250],
+                [0.4219, 1.0000, 0.4219, 0.1250, 0.0156],
+                [1.0000, 0.4219, 0.1250, 0.0156, 0.0000],
+            ],
+        ], dtype=np.float32)
+        self.run_test(exp, ease)
+
+
+class EaseInElasticTestCase(EaseTestCase):
+    def test_ease(self):
+        """Given an array of image data, run the 'in elastic' easing
+        function on the data and return the result.
+        """
+        ease = ie.ease_in_elastic
+        exp = np.array([
+            [
+                [0.0000, -0.0055, -0.0156, 0.0884, 1.0000],
+                [-0.0055, -0.0156, 0.0884, 1.0000, 0.0884],
+                [-0.0156, 0.0884, 1.0000, 0.0884, -0.0156],
+                [0.0884, 1.0000, 0.0884, -0.0156, -0.0055],
+                [1.0000, 0.0884, -0.0156, -0.0055, 0.0000],
+            ],
+        ], dtype=np.float32)
+        self.run_test(exp, ease)
+
+
+class EaseInQuadTestCase(EaseTestCase):
+    def test_ease(self):
+        """Given an array of image data, run the 'in quad' easing
+        function on the data and return the result.
+        """
+        ease = ie.ease_in_quad
+        exp = np.array([
+            [
+                [0.0000, 0.0625, 0.2500, 0.5625, 1.0000],
+                [0.0625, 0.2500, 0.5625, 1.0000, 0.5625],
+                [0.2500, 0.5625, 1.0000, 0.5625, 0.2500],
+                [0.5625, 1.0000, 0.5625, 0.2500, 0.0625],
+                [1.0000, 0.5625, 0.2500, 0.0625, 0.0000],
+            ],
+        ], dtype=np.float32)
+        self.run_test(exp, ease)
+
+
+class EaseInQuintTestCase(EaseTestCase):
+    def test_ease(self):
+        """Given an array of image data, run the 'in quint' easing
+        function on the data and return the result.
+        """
+        ease = ie.ease_in_quint
+        exp = np.array([
+            [
+                [0.0000, 0.0010, 0.0312, 0.2373, 1.0000],
+                [0.0010, 0.0312, 0.2373, 1.0000, 0.2373],
+                [0.0312, 0.2373, 1.0000, 0.2373, 0.0312],
+                [0.2373, 1.0000, 0.2373, 0.0312, 0.0010],
+                [1.0000, 0.2373, 0.0312, 0.0010, 0.0000],
+            ],
+        ], dtype=np.float32)
+        self.run_test(exp, ease)
+
+
 class EaseInOutSinTestCase(EaseTestCase):
     def test_ease(self):
-        """Given an array of image data, run the 'in sine' easing
+        """Given an array of image data, run the 'in out sine' easing
         function on the data and return the result.
         """
         ease = ie.ease_in_out_sin
@@ -45,7 +153,7 @@ class EaseInOutSinTestCase(EaseTestCase):
 
 class EaseInSinTestCase(EaseTestCase):
     def test_ease(self):
-        """Given an array of image data, run the 'in sine' easing
+        """Given an array of image data, run the 'out sine' easing
         function on the data and return the result.
         """
         ease = ie.ease_in_sin
